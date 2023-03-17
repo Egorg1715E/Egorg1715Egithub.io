ff70dbfc9614f46f1e4719075da994ccfbd67b44
rqwer <egorg1775@gmail.com>
2023-03-17 12:12:10 +0300

    C#

---
+++ MathTaskClassLibrary/MathTaskClassLibraryTests/GeometryTests.cs
@@ -0,0 +1,26 @@
+﻿using MathTaskClassLibrary;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            //Исходные данные
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            //Полученные значения с помощью тестируемого
+            Geometry g = new Geometry();
+            int actual = g.RectangleArea(a, b);
+
+            //Сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
